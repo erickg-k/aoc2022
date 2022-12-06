e6ae323be5f27356e989c43c8be24e19a0249c57
Erick Guan <erickguan@users.noreply.github.com>
2022-12-06 23:47:46 +0100

    day4-1

---
+++ src/day4.rs
@@ -0,0 +1,35 @@
+use std::io;
+use std::vec::Vec;
+
+fn analyze_assignment(assignment: &str) -> (i32, Vec<i32>) {
+    let seg: Vec<i32> = assignment
+        .split('-')
+        .map(|e| e.parse::<i32>().expect("parse as an i32"))
+        .collect();
+    assert_eq!(seg.len(), 2);
+    return (seg[1] - seg[0] + 1, seg);
+}
+
+fn segment_inclusion(segment: &str) -> bool {
+    let pairs: Vec<&str> = segment.split(',').collect();
+    assert_eq!(pairs.len(), 2);
+    let (len1, seg1) = analyze_assignment(pairs[0]);
+    let (len2, seg2) = analyze_assignment(pairs[1]);
+    if len1 <= len2 {
+        return seg2[1] >= seg1[1] && seg2[0] <= seg1[0];
+    } else {
+        return seg1[1] >= seg2[1] && seg1[0] <= seg2[0];
+    }
+}
+
+pub fn count_segments_inclusions() {
+    let mut cnt = 0;
+    for line in io::stdin().lines() {
+        let line_str = line.expect("IO failed reading data");
+
+        if segment_inclusion(line_str.as_str()) {
+            cnt += 1;
+        }
+    }
+    println!("Count: {cnt}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate aoc2022;
 
 fn main() {
-    aoc2022::day3::get_priorities_owned_by_3_elf();
+    aoc2022::day4::count_segments_inclusions();
 }
