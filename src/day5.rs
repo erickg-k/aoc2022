f449b3ac2cca58c9c8a239516e03b684b0ba279f
Erick Guan <erick.guan@klarna.com>
2022-12-07 14:00:06 +0100

    day5-2

--- src/day5.rs
+++ src/day5.rs
@@ -71,3 +71,45 @@ pub fn top_crate_after_moving() {
     let top_crates = top_crates_in_stack(&stacks);
     println!("Top crates: {top_crates}");
 }
+
+pub fn top_crate_after_moving_with_new_crane() {
+    let mut state = State::INITIAL;
+    let mut stack_map = Vec::new();
+    let mut stacks = Vec::new();
+
+    for line in io::stdin().lines() {
+        let line_str = line.expect("IO failed reading data");
+
+        if line_str == "" {
+            craete_stack(&mut stacks, &mut stack_map);
+            state = State::INSTRUCTION;
+            continue;
+        }
+
+        match state {
+            State::INITIAL => {
+                stack_map.push(line_str);
+            }
+            State::INSTRUCTION => {
+                let c = line_str
+                    .as_str()
+                    .split_whitespace()
+                    .map(|e| e.parse::<i32>())
+                    .filter(|e| e.is_ok())
+                    .map(|e| e.unwrap() as usize)
+                    .collect::<Vec<_>>();
+                let (num_move, crate_from_id, crate_to_id) = (c[0], c[1], c[2]);
+                let mut stage = Vec::new();
+                for _ in 0..num_move {
+                    let crate_ = stacks[crate_from_id - 1].pop().unwrap();
+                    stage.push(crate_);
+                }
+                while let Some(c) = stage.pop() {
+                    stacks[crate_to_id - 1].push(c);
+                }
+            }
+        }
+    }
+    let top_crates = top_crates_in_stack(&stacks);
+    println!("Top crates: {top_crates}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate aoc2022;
 
 fn main() {
-    aoc2022::day5::top_crate_after_moving();
+    aoc2022::day5::top_crate_after_moving_with_new_crane();
 }
